d7c6e8f7042b69ac804c6f885f1c637bfc7a1e4f
Luisa <marialuisamanzanom@gmail.com>
2023-05-18 14:15:41 -0600

    logica resuelta encriptar y desencriptar

--- encriptador.js
+++ encriptador.js
@@ -1,43 +1,65 @@
 
 function encriptarTextoInput(textoInput,separador){
+    textoInput = textoInput.toLowerCase();
     let arrayInput = textoInput.split(separador);
     console.log("String input fue: " + textoInput + '"');
-    console.log('El separador es: "' + separador + '"');
 
     for (let i = 0; i < arrayInput.length; i++){
         if (arrayInput[i] == "a") {
             let nuevoArray = arrayInput[i].replace(letraA, "ai");
-            console.log(nuevoArray);
+            textoEncriptado += nuevoArray
         }
         else if (arrayInput[i] == "e") {
             let nuevoArray = arrayInput[i].replace(letraE, "enter");
-            console.log(nuevoArray);
+            textoEncriptado += nuevoArray
         }
         else if (arrayInput[i] == "i") {
             let nuevoArray = arrayInput[i].replace(letraI, "imes");
-            console.log(nuevoArray);
+            textoEncriptado += nuevoArray
         }
         else if (arrayInput[i] == "o") {
             let nuevoArray = arrayInput[i].replace(letraO, "ober");
-            console.log(nuevoArray);
+            textoEncriptado += nuevoArray
         }
         else if (arrayInput[i] == "u") {
             let nuevoArray = arrayInput[i].replace(letraU, "ufat");
-            console.log(nuevoArray);
+            textoEncriptado += nuevoArray
         } 
         else {
-            console.log(arrayInput[i]);
+            textoEncriptado += arrayInput[i]
         }
     }
 }
 
-let string1 = "A a e azul o a e verde i i hola o u";
-let string2 = "El gato verde marciando vuela sobre guadalajara"
-let separadorEspacio = " ";
-let letraA = /a*/gi
-let letraE = /e*/gi
-let letraI = /i*/gi
-let letraO = /o*/gi
-let letraU = /u*/gi
+let string1 = "El gato verde marciando vuela sobre guadalajara";
+let separadorEspacio = "";
+let textoEncriptado = "";
+let letraA = /a/gi
+let letraE = /e/gi
+let letraI = /i/gi
+let letraO = /o/gi
+let letraU = /u/gi
 
 encriptarTextoInput(string1,separadorEspacio);
+console.log("El texto encriptado es: " + textoEncriptado);
+console.log("----------------");
+
+
+//Desencriptar \ ^
+
+let ai = /ai/gi
+let enter = /enter/gi
+let imes = /imes/gi
+let ober = /ober/gi
+let ufat = /ufat/gi
+
+let stringEncriptado = "enterl gaitober venterrdenter maircimesaindober vufatenterlai soberbrenter gufataidailaijairai";
+let desencriptarTextoA = stringEncriptado.replace(ai, "a");
+let desencriptarTextoE = desencriptarTextoA.replace(enter,"e");
+let desencriptarTextoI = desencriptarTextoE.replace(imes, "i");
+let desencriptarTextoO = desencriptarTextoI.replace(ober, "o");
+let desencriptarTextoU = desencriptarTextoO.replace(ufat, "u");
+
+console.log("El texto encriptado es: " + stringEncriptado)
+console.log("El texto desencriptado es: " + desencriptarTextoU);
+console.log("----------------");
